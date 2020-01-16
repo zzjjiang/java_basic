71d202fefd81e35ea562a7134e587219a203b5d1
zzjjiang <185134019@qq.com>
2020-01-16 15:49:24 +0800

    design

---
+++ src/main/java/com/zzj/designPattern/entrust/Company.java
@@ -0,0 +1,12 @@
+package com.zzj.designPattern.entrust;
+
+/**
+ * 这里用一句话描述这个类的作用
+ *
+ * @author jone
+ * @date: 2020/01/16 15:44
+ * @since JDK 1.8
+ */
+public interface Company {
+	void product();
+}
---
+++ src/main/java/com/zzj/designPattern/template/LiSon.java
@@ -0,0 +1,15 @@
+package com.zzj.designPattern.template;
+
+/**
+ * 这里用一句话描述这个类的作用
+ *
+ * @author jone
+ * @date: 2020/01/16 15:37
+ * @since JDK 1.8
+ */
+public class LiSon extends Father {
+	@Override
+	public void love() {
+		System.out.println("Li love");
+	}
+}
