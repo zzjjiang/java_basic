0093e2423078b4c56cfed04d377569cacdef0022
zujiangzou <185134019@qq.com>
2020-02-11 15:56:55 +0800

    reflect 注释补充

--- src/main/java/com/zzj/reflect/ReflectTest.java
+++ src/main/java/com/zzj/reflect/ReflectTest.java
@@ -8,6 +8,24 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
+/**
+ * 区别一
+ *
+ * 　　在方法名末尾有s的是返回一个数组，没有s的是返回单个构造器。
+ *
+ * 区别二
+ *
+ * 　　在方法名中加Declared的是返回所有的构造方法，不加Declared的只返回public访问权限的构造器
+ *
+ * 区别三
+ *
+ * 　　有参数的都是在方法名中没有在结尾处加s的，返回的是指定参数类型的构造器
+ *
+ *
+ *
+ * 总结：反射机制中，所有添加Declared的获取方式都是暴力获取所有构造（或方法，或字段），通过暴力获取的字段我们在进行访问的时候需要进行可访问性设置，即
+ *
+ */
 public class ReflectTest {
 
     @Test
